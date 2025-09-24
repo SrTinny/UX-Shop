6874ca10741e1330650093c22fbad6e9f344b286
João Victor Duarte <joaovictorcardosoduartejoao@gmail.com>
2025-09-24 19:34:25 -0300

    chore(backend): configura middleware CORS no servidor Express
    - Configura CORS para permitir requisições vindas das portas 3000 e 3001 (localhost)
    - Suporta métodos HTTP e cabeçalhos comuns
    - Permite que aplicações frontend interajam com a API do backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,29 @@
 // src/index.ts
 import express from 'express';
+import cors from 'cors';
 import productRoutes from './modules/products/product.routes';
 import authRoutes from './modules/auth/auth.routes';
-import { errorHandler } from './middlewares/errorHandler';
 import cartRoutes from './modules/cart/cart.routes';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:3001'], // Next em 3000 ou 3001
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+// 2) body parser
 app.use(express.json());
 
-// auth
+// 3) rotas
 app.use('/auth', authRoutes);
-
-// products
 app.use('/products', productRoutes);
-
-// cart
 app.use('/cart', cartRoutes);
 
-
+// 4) health e error handler
 app.get('/', (_req, res) => res.send('🚀 Servidor rodando com TypeScript!'));
 app.use(errorHandler);
 
